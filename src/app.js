8004786ee73039b0c66a91c16d5e3972eb779c47
Bronson Dunbar <bronson@bronsondunbar.com>
2019-05-20 16:57:53 +0200

    Updates

--- src/app.js
+++ src/app.js
@@ -21,11 +21,26 @@ class App extends Component {
 
   async componentDidMount () {
     const { dispatch } = this.props
-    fetch("https://api.thecatapi.com/api/images/get?format=json&results_per_page=10&size=small&type=png")
+    // fetch("https://api.thecatapi.com/api/images/get?format=json&results_per_page=10&size=small&type=png")
+    //   .then(function(response) {
+    //     return response.json()
+    //   })
+    //   .then(function(data) {
+    //     dispatch(getItems(data))
+    //   })
+
+    const request = new Request('https://api.thecatapi.com/v1/breeds?limit=250', {
+      headers: new Headers({
+        'x-api-key': '15bd9057-cbff-4df5-a01c-bc875c2e55a2'
+      })
+    })
+
+    fetch(request)
       .then(function(response) {
         return response.json()
       })
       .then(function(data) {
+        console.log(data)
         dispatch(getItems(data))
       })
   }
